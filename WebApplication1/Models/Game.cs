679a0fd8e49422b0b24c06e6b58555f788187637
Brenton Phillips <phillips.brenton@gmail.com>
2020-08-13 08:54:45 -0500

    created new App with Crud

---
+++ WebApplication1/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebApplication1.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public int MinimumPlayers { get; set; }
+        public int MaximumPlayers { get; set; }
+    }
+}
